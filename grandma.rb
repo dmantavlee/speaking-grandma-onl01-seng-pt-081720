dea96704aade6e336090129006cadf7a7433d465
Jason Beckstead <jason_beckstead@yahoo.com>
2020-08-18 23:30:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@ def speak_to_grandma(phrase == "I LOVE YOU TO PUMKIN!")
 if speak_to_grandma == "Hi Nana, how are you?"
   puts "HUH? ! SPEAK UP, SONNY!"
   elsif speak_to_grandma == ""
-    
+    binding.pry
   end
 
  
