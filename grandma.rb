a2d8f476d760dd70035036650e995680a11acf7a
Jason Beckstead <jason_beckstead@yahoo.com>
2020-08-18 23:28:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
-speak_to_grandma == "I LOVE YOU TO PUMKIN!"
+def speak_to_grandma(phrase == "I LOVE YOU TO PUMKIN!")
 if speak_to_grandma == "Hi Nana, how are you?"
   puts "HUH? ! SPEAK UP, SONNY!"
   elsif speak_to_grandma == ""
     
-    
+  end
 
  
 
